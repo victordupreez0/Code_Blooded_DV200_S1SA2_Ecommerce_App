757688b7cfcc16ee30de1a2d9de7a65273b2ec4a
Victor DP <victor.dupreez0@gmail.com>
2025-06-05 11:48:52 +0200

    Admin access control

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,7 +4,8 @@ const bcrypt = require('bcrypt');
 const UserSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   email:    { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' } // Add role field
 });
 
 // Hash password before saving
